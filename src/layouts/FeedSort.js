146b9bb6abf75526c92219067163027b86c01782
aplyd <austin@ftacnik.com>
2020-04-20 21:00:39 -0700

    refactor styled components

--- src/layouts/FeedSort.js
+++ src/layouts/FeedSort.js
@@ -7,47 +7,47 @@ const Container = styled.div`
 	grid-column: 1;
 	height: 54px;
 	${roundedGreyBorder}
+`;
 
-	&& > div > label {
-		color: ${(props) => props.theme.colors.gray};
-		&:hover {
-			color: ${(props) => props.theme.colors.blue};
-			cursor: pointer;
-		}
-		&:checked {
-			color: ${(props) => props.theme.colors.blue};
-			cursor: pointer;
-		}
-	}
+const HiddenInput = styled.input`
+	display: none;
+`;
 
-	&& > div > label > input {
-		display: none;
+const Label = styled.label`
+	color: ${(props) => props.theme.colors.gray};
+	&:hover {
+		color: ${(props) => props.theme.colors.blue};
+		cursor: pointer;
 	}
 `;
+
+const NewLabel = styled(Label)``;
+const OldLabel = styled(Label)``;
+const TopLabel = styled(Label)``;
 
 export default function FeedSort({ sortPosts }) {
 	return (
 		<Container>
 			<div onChange={(e) => sortPosts(e.target.value)}>
-				<label>
+				<NewLabel style={{}}>
 					new
-					<input type="radio" name="sort" value="TIME_ASC" />
+					<HiddenInput type="radio" name="sort" value="TIME_ASC" />
 					<span className="difficulty-radio"></span>
-				</label>
-				<label
+				</NewLabel>
+				<OldLabel
 				// style={this.state.difficulty === 'medium' ? selected : null}
 				>
 					old
-					<input type="radio" name="sort" value="TIME_DESC" />
+					<HiddenInput type="radio" name="sort" value="TIME_DESC" />
 					<span className="difficulty-radio"></span>
-				</label>
-				<label
+				</OldLabel>
+				<TopLabel
 				// style={this.state.difficulty === 'hard' ? selected : null}
 				>
 					top
-					<input type="radio" name="sort" value="VOTE_ASC" />
+					<HiddenInput type="radio" name="sort" value="VOTE_ASC" />
 					<span className="difficulty-radio"></span>
-				</label>
+				</TopLabel>
 			</div>
 		</Container>
 	);
