2362d3fbd3efa1a0cec7dc66fde8e2596ecd6a11
aplyd <austin@ftacnik.com>
2020-04-24 10:54:15 -0700

    fix submit page modal issue

--- src/pages/Submit.js
+++ src/pages/Submit.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Container } from '../App';
-import Nav from '../layouts/Nav';
 import CreatePost from '../components/CreatePost';
 
 export default function Submit({ setUpdatePosts }) {
 	return (
 		<Container>
-			<Nav />
 			<CreatePost setUpdatePosts={setUpdatePosts} />
 		</Container>
 	);
