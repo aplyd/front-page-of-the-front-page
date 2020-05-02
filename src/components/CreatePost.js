8bc61f0be5cda781e0a031cdca93b2b204756d1f
aplyd <austin@ftacnik.com>
2020-05-02 14:24:41 -0700

    submit/create post styling

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -6,30 +6,59 @@ import { PostContext } from '../PostContext';
 import { v4 as uuidv4 } from 'uuid';
 
 const FormContainer = styled.form`
-	padding: 20px;
-	display: flex;
-	flex-direction: column;
+	padding: 12 0px;
 `;
 
-const TitleInput = styled.input``;
-const TextInput = styled.textarea``;
-const Button = styled.button`
-	width: auto;
-	height: auto;
+const TitleInput = styled.input`
+	width: 100%;
+	text-indent: 10px;
 	border-radius: 4px;
-	padding: 3px 16px 3px 16px;
-	font-size: 12px;
-	text-align: center;
-	text-transform: uppercase;
-	line-height: 24px;
-	margin: 0 4px;
-	&&:hover {
-		cursor: pointer;
+	font-size: 16px;
+	outline: none;
+	border: none;
+	padding: 8px;
+	margin-bottom: 16px;
+	&&:focus,
+	:hover {
+		border: solid 0.5px black;
+	}
+`;
+const TextInput = styled.textarea`
+	width: 100%;
+	padding: 18px 12px 12px 12px;
+	text-indent: 10px;
+	border-radius: 4px;
+	min-height: 122px;
+	font-size: 16px;
+	outline: none;
+	border: none;
+	&&:focus,
+	:hover {
+		border: solid 0.5px black;
 	}
 `;
 
-const CancelBtn = styled(Button)``;
-const SubmitBtn = styled(Button)``;
+const CancelBtn = styled.button`
+	color: ${(props) => props.theme.colors.blue};
+	background-color: white;
+	border: 1px solid ${(props) => props.theme.colors.blue};
+	line-height: 24px;
+	:hover {
+		color: ${(props) => props.theme.colors.lightBlue};
+		border: 1px solid ${(props) => props.theme.colors.lightBlue};
+	}
+	display: inline;
+`;
+const SubmitBtn = styled.button`
+	color: white;
+	background-color: ${(props) => props.theme.colors.blue};
+	border: 1px solid white;
+	line-height: 24px;
+	:hover {
+		background-color: ${(props) => props.theme.colors.lightBlue};
+	}
+	display: inline;
+`;
 
 //TODO - trigger rerender of App.js after posting ??
 export default function CreatePost({ props, setUpdatePosts }) {
@@ -58,6 +87,7 @@ export default function CreatePost({ props, setUpdatePosts }) {
 					id: postID,
 				})
 				.then(() => {
+					//this is probably bad
 					setUpdatePosts(Date.now());
 					history.push('/');
 				});
@@ -79,6 +109,7 @@ export default function CreatePost({ props, setUpdatePosts }) {
 					],
 				});
 
+			//is this neeeded????
 			firebase
 				.firestore()
 				.collection('users')
@@ -105,6 +136,8 @@ export default function CreatePost({ props, setUpdatePosts }) {
 
 			setTitle('');
 			setPostText('');
+		} else {
+			alert('you must sign in to create a post :)');
 		}
 	};
 
@@ -115,11 +148,15 @@ export default function CreatePost({ props, setUpdatePosts }) {
 				value={title}
 				required
 				onChange={(e) => setTitle(e.currentTarget.value)}
+				placeholder="Title"
+				maxLength="300"
 			></TitleInput>
 			<TextInput
 				value={postText}
 				required
 				onChange={(e) => setPostText(e.currentTarget.value)}
+				placeholder="Text (optional)"
+				maxLength="2000"
 			></TextInput>
 			<CancelBtn type="button">Cancel</CancelBtn>
 			<SubmitBtn type="submit">Post</SubmitBtn>
--- src/pages/Submit.js
+++ src/pages/Submit.js
@@ -11,25 +11,29 @@ const CreatePostContainer = styled.div`
 	grid-row: 1;
 `;
 
-const CreateAPost = styled.h3``;
+const CreateAPost = styled.h3`
+	padding-left: 4px;
+`;
 
 const WhiteLine = styled.div`
 	border-radius: 4px;
 	background: white;
-	height: 1px;
+	height: 2px;
+	border-radius: 4px;
 	margin-top: 4px;
 	margin-bottom: 16px;
 `;
 
 const PostP = styled.p`
 	color: ${(props) => props.theme.colors.blue};
-	padding: 24px 0 12px 56px;
+	padding: 24px 0 12px 10%;
 `;
 
 const BlueLine = styled.div`
 	background-color: ${(props) => props.theme.colors.blue};
 	height: 2px;
 	width: 25%;
+	margin-bottom: 8px;
 `;
 
 export default function Submit({ setUpdatePosts }) {
