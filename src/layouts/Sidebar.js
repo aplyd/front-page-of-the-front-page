4be77128dcb5052dd4095bb0eacd3405f75dd808
aplyd <austin@ftacnik.com>
2020-05-06 20:43:03 -0700

    more sidebar styling and content

--- src/layouts/Sidebar.js
+++ src/layouts/Sidebar.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import * as S from './Sidebar.style';
 import firebase from '../firebase';
+import { GiCakeSlice } from 'react-icons/gi';
 
 export default function Sidebar() {
 	const [userCount, setUserCount] = useState();
@@ -18,19 +19,57 @@ export default function Sidebar() {
 
 	return (
 		<S.Container>
-			<S.AboutCommunityDiv>
-				<p>About Community</p>
-			</S.AboutCommunityDiv>
-			<S.Description>
-				Welcome to the front page of the front page! This was built by
-				me, Austin Ftacnik, for my final project of The Odin Project
-				JavaScript course. If you haven't figured it out yet, this is a
-				simplified clone of popular website and was built for learning
-				purposes, not to replace said site. If you have any questions or
-				comments, feel free to contact via Github or Twitter. <br />
-				<br />
-				Cheers.
-			</S.Description>
+			<S.AboutContainer>
+				<S.AboutProjectDiv>
+					<p>About Project</p>
+				</S.AboutProjectDiv>
+				<S.Description>
+					Welcome to the front page of the front page! This was built
+					by me, Austin Ftacnik, for my final project of{' '}
+					<a
+						href="https://www.theodinproject.com/courses/javascript/lessons/final-project-116ff273-1e55-4055-bd7f-146c17d0ec9c"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						The Odin Project
+					</a>{' '}
+					JavaScript course. This is a simplified clone of popular
+					website and was built for learning purposes, not to replace
+					said site. If you have any questions or comments, feel free
+					to contact via{' '}
+					<a
+						href="https://github.com/aplyd"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Github
+					</a>{' '}
+					or{' '}
+					<a
+						href="https://twitter.com/austinftacnik"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Twitter
+					</a>
+					Cheers.
+				</S.Description>
+				<div>
+					<S.UserCount>
+						<S.MembersNum>12</S.MembersNum>
+						<S.Members>Members</S.Members>
+					</S.UserCount>
+					<S.PostCount>
+						<S.PostsNum>16</S.PostsNum>
+						<S.Posts>Posts</S.Posts>
+					</S.PostCount>
+				</div>
+
+				<S.CreationDate>
+					<S.SVG as={GiCakeSlice} /> <p>Created April 15th, 2020</p>
+				</S.CreationDate>
+				<S.Spacer height={24} />
+			</S.AboutContainer>
 		</S.Container>
 	);
 }
--- src/layouts/Sidebar.style.js
+++ src/layouts/Sidebar.style.js
@@ -1,14 +1,17 @@
 import styled from 'styled-components';
+import { roundedGreyBorder } from '../GlobalStyle';
 
 const Container = styled.div`
 	grid-column: 2;
-	grid-row: 1 / span 2;
-	background: white;
-	border: 1px solid lightgray;
-	border-radius: 4px;
+	grid-row: 1 / 3;
 `;
 
-const AboutCommunityDiv = styled.div`
+const AboutContainer = styled.div`
+	background: white;
+	${roundedGreyBorder};
+`;
+
+const AboutProjectDiv = styled.div`
 	background: ${(props) => props.theme.colors.lightGray};
 	border-top-right-radius: 4px;
 	border-top-left-radius: 4px;
@@ -23,6 +26,56 @@ const Description = styled.p`
 	color: black;
 	font-size: 13px;
 	line-height: 1.4em;
+
+	&& > a {
+		color: ${(props) => props.theme.colors.blue};
+	}
 `;
 
-export { Container, AboutCommunityDiv, Description };
+const UserCount = styled.div`
+	display: inline-block;
+	padding: 12px;
+`;
+
+const Members = styled.p``;
+const MembersNum = styled.p``;
+
+const PostCount = styled.div`
+	display: inline-block;
+	padding: 12px;
+`;
+
+const Posts = styled.p``;
+const PostsNum = styled.p``;
+
+const CreationDate = styled.div`
+	display: inline;
+	padding: 12px 12px 0 12px;
+	&& > p {
+		display: inline;
+	}
+`;
+
+const SVG = styled.svg`
+	transform: scaleX(-1);
+`;
+
+const Spacer = styled.div`
+	height: ${(props) => props.height + 'px'};
+`;
+
+export {
+	Container,
+	AboutContainer,
+	AboutProjectDiv,
+	Description,
+	UserCount,
+	PostCount,
+	CreationDate,
+	SVG,
+	Members,
+	MembersNum,
+	Posts,
+	PostsNum,
+	Spacer,
+};
