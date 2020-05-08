0a77d46924f41cf5814b23654094cd096c19cd91
aplyd <austin@ftacnik.com>
2020-05-07 21:44:42 -0700

    begin rearranging file structure

--- src/layouts/Sidebar.js
+++ src/layouts/Sidebar.js
@@ -1,13 +1,16 @@
-import React, { useEffect, useState, useContext } from 'react';
-import * as S from './Sidebar.style';
-import firebase from '../firebase';
-import { GiCakeSlice } from 'react-icons/gi';
-import { PostContext } from '../PostContext';
+import React from 'react';
+import styled from 'styled-components';
+import SideBarAboutProject from '../components/SidebarAboutProject/SidebarAboutProject';
+
+//to keep the S.Component naming convention for styled components consistent
+const S = {};
+
+S.Container = styled.div`
+	grid-column: 2;
+	grid-row: 1 / 3;
+`;
 
 export default function Sidebar() {
-	const [userCount, setUserCount] = useState();
-	const { posts } = useContext(PostContext);
-
 	//TODO - get user count and post count to display in sidebar
 	// useEffect(() => {
 	// 	let mounted = true;
@@ -32,61 +35,7 @@ export default function Sidebar() {
 
 	return (
 		<S.Container>
-			<S.AboutContainer>
-				<S.AboutProjectDiv>
-					<p>About Project</p>
-				</S.AboutProjectDiv>
-				<S.Spacer height={12} />
-				<S.Description>
-					Welcome to the front page of the front page! This was built
-					by me, Austin Ftacnik, for my final project of{' '}
-					<a
-						href="https://www.theodinproject.com/courses/javascript/lessons/final-project-116ff273-1e55-4055-bd7f-146c17d0ec9c"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						The Odin Project
-					</a>{' '}
-					JavaScript course. This is a simplified clone of popular
-					website and was built for learning purposes, not to replace
-					said site. If you have any questions or comments, feel free
-					to contact via{' '}
-					<a
-						href="https://github.com/aplyd"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Github
-					</a>{' '}
-					or{' '}
-					<a
-						href="https://twitter.com/austinftacnik"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Twitter
-					</a>
-					.
-				</S.Description>
-				<div>
-					<S.UserCount>
-						<S.MembersNum>
-							{userCount ? userCount : '2'}
-						</S.MembersNum>
-						<S.Members>Members</S.Members>
-					</S.UserCount>
-					<S.PostCount>
-						<S.PostsNum>16</S.PostsNum>
-						<S.Posts>Posts</S.Posts>
-					</S.PostCount>
-				</div>
-
-				<S.Spacer height={12} />
-				<S.CreationDate>
-					<S.SVG as={GiCakeSlice} /> <p>Created April 15th, 2020</p>
-				</S.CreationDate>
-				<S.Spacer height={24} />
-			</S.AboutContainer>
+			<SideBarAboutProject />
 		</S.Container>
 	);
 }
