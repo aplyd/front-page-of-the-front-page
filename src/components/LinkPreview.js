f7f1f6a9dfc1c3c3711ad44720df8110f8ac9ded
aplyd <austin@ftacnik.com>
2020-05-28 21:00:32 -0700

    display link previews

--- src/components/LinkPreview.js
+++ src/components/LinkPreview.js
@@ -1,43 +1,66 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { RiLinksLine } from 'react-icons/ri';
 
+const Container = styled.div`
+	padding: 4px 164px 20px 8px;
+	display: flex;
+	flex-direction: column;
+`;
+
 const PreviewImage = styled.div`
 	position: absolute;
-	right: 0;
-	top: 0;
+	right: 8px;
+	top: 16px;
 	width: 142px;
 	height: 98px;
 	border-radius: 4px;
-	background-image: ${(props) => `url('${props.img}')`};
 	color: ${(props) => props.theme.colors.blue};
 	border: solid 0.5px ${(props) => props.theme.colors.blue};
+	background-image: ${(props) => 'url(' + props.img + ')'};
+	background-size: contain;
+	background-position: center;
 `;
 
 const PreviewImageFallback = styled(PreviewImage)`
-	background: ${(props) => props.theme.colors.lightGray};
 	display: flex;
 	align-items: center;
 	justify-content: center;
 `;
 
 const LinkTitle = styled.h2`
-	padding: 4px 134px 20px 8px;
 	font-size: 20px;
 `;
 
-const SVG = styled.svg``;
+const SVG = styled.svg`
+	font-size: 24px;
+`;
 
-const Link = styled.a``;
+const Link = styled.a`
+	font-size: 12px;
+	justify-self: flex-end;
+	padding-top: 4px;
+	color: ${(props) => props.theme.colors.blue};
+	&&:visited {
+		color: ${(props) => props.theme.colors.blue};
+	}
+`;
 
 export default function LinkPreview({ title, url, preview }) {
+	const [img, setImg] = useState(null);
+
+	useEffect(() => {
+		console.log(preview);
+		if (preview !== null || preview !== undefined) {
+			setImg(preview);
+		}
+	}, [preview]);
+
 	return (
-		<div>
+		<Container>
 			<LinkTitle>{title}</LinkTitle>
 			{preview ? (
-				<PreviewImage>
-					<img alt={`link to: ${url}`} src={url}></img>
-				</PreviewImage>
+				<PreviewImage img={img}></PreviewImage>
 			) : (
 				<PreviewImageFallback>
 					<SVG as={RiLinksLine} />{' '}
@@ -50,6 +73,6 @@ export default function LinkPreview({ title, url, preview }) {
 			>
 				{url}
 			</Link>
-		</div>
+		</Container>
 	);
 }
